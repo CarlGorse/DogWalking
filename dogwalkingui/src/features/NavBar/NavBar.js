eff505ece09df9caeed61bc053cd82b3c9fdc35c
CarlGorse <carl.gorse@hotmail.co.uk>
2023-04-03 21:07:39 +0100

    Make menu responsive

--- dogwalkingui/src/features/NavBar/NavBar.js
+++ dogwalkingui/src/features/NavBar/NavBar.js
@@ -1,29 +1,24 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand='sm' bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="./home">Dog walking</Navbar.Brand>
-        <Nav className="me-auto">
-          <NavDropdown title="Bookings">
-            <NavDropdown.Item href="./adhocBookingPage">New adhoc booking</NavDropdown.Item>
-          </NavDropdown>
-
-          <Nav.Link href="./userSettings">Settings</Nav.Link>
-          <Nav.Link href="./adminGeneralSettings">Admin settings</Nav.Link>
-          {/*<NavDropdown title="Admin settings">
-            <NavDropdown.Item href="./adminGeneralSettings">General</NavDropdown.Item>
-            <NavDropdown.Item href="./adminPlannedLocations">Planned locations</NavDropdown.Item>
-        </NavDropdown>
-        */}
-          <Nav.Link href="./contact">Contact</Nav.Link>
-          <Nav.Link href="./about">About</Nav.Link>
-        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <NavDropdown title="Bookings">
+              <NavDropdown.Item href="./adhocBookingPage">New adhoc booking</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="./userSettings">Settings</Nav.Link>
+            <Nav.Link href="./adminGeneralSettings">Admin settings</Nav.Link>
+            <Nav.Link href="./contact">Contact</Nav.Link>
+            <Nav.Link href="./about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar >
   );
