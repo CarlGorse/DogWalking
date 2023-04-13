e15b335d9035b0e653ad967bd661bb2268342d65
CarlGorse <carl.gorse@hotmail.co.uk>
2023-04-13 10:41:50 +0100

    fix issue with existing dogs having no id

--- dogwalkingui/src/components/Data/UserSettings.js
+++ dogwalkingui/src/components/Data/UserSettings.js
@@ -3,8 +3,8 @@ const userSettings =
   selectService: { typeOfWalk: 'Any' },
   aboutYou: { name: 'Carl', postCode: 'BA1 1AA', locationDetails: 'I live on a hill.' },
   aboutYourDogs: [
-    { name: 'Alfie', breed: 'Cockapoo', size: 'Medium', canBeOffLead: 'Either', comments: null },
-    { name: 'River', breed: 'King Charles', size: 'Small', canBeOffLead: 'No', comments: 'Shits a lot.' }
+    { id: 0, name: 'Alfie', breed: 'Cockapoo', size: 'Medium', canBeOffLead: 'Either', comments: null },
+    { id: 1, name: 'River', breed: 'King Charles', size: 'Small', canBeOffLead: 'No', comments: 'Shits a lot.' }
   ],
   options: {
     canOtherDogJoin: 'Yes'
