29d7c5bdbb729f2c8cabb08dc15884cd0e2a56e6
Carl Gorse <carl.gorse@hotmail.co.uk>
2023-05-05 17:26:24 +0100

    tests

---
+++ api/IntegrationTests.cs/TimeslotServiceTests/BookTimeslotsTests.cs
@@ -0,0 +1,64 @@
+﻿using DogWalkingApi.DbContext;
+using DogWalkingApi.Repositories;
+using DogWalkingApi.Services;
+using DogWalkingApi.Types;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    [TestFixture]
+    public class BookTimeslotsTests
+    {
+
+        [Test]
+        public void IdsHasKnownTimeslot_TimeslotHasBooking()
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("DogWalking").Options;
+
+            var dbContext = new DogWalkingDbContext(options);
+            var timeslot = new Timeslot();
+            dbContext.Timeslots.Add(timeslot);
+            
+            dbContext.SaveChanges();
+
+            Assert.IsNull(dbContext.Timeslots.Single().Booking);
+
+            var timeslotService = new TimeslotService(
+                new TimeslotRepository(dbContext),
+                new BookingTimeslotRepository(dbContext));
+
+            var booking = new Booking();
+            
+            timeslotService.BookTimeslots(booking, timeslotIds: new int[] { timeslot.TimeslotId });
+
+            Assert.AreEqual(booking, dbContext.Timeslots.Single().Booking);
+        }
+
+        /*
+        [Test]
+        public void IdsHasUnknownTimeslot_BookingNotAddedToAnyTimeslot()
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("DogWalking").Options;
+
+            var dbContext = new DogWalkingDbContext(options);
+            var timeslot = new Timeslot();
+            dbContext.Timeslots.Add(timeslot);
+
+            dbContext.SaveChanges();
+
+            Assert.IsNull(dbContext.Timeslots.Single().Booking);
+
+            var timeslotService = new TimeslotService(
+                new TimeslotRepository(dbContext),
+                new BookingTimeslotRepository(dbContext));
+
+            var booking = new Booking();
+
+            timeslotService.BookTimeslots(booking, timeslotIds: new int[] { timeslot.TimeslotId });
+
+            Assert.AreEqual(booking, dbContext.Timeslots.Single().Booking);
+        }
+        */
+    }
+}
